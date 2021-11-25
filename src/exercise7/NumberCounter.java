7776f04f31a127019c0a3e4f7c83551c35f1fcad
NNikolov02 <>
2021-11-25 10:38:17 +0000

    Ubung7

--- src/exercise7/NumberCounter.java
+++ src/exercise7/NumberCounter.java
@@ -19,12 +19,23 @@ public class NumberCounter {
         System.out.print("Search for number: ");
         int number = input.nextInt();
 
-        int numberOccurrences = 0; // Call method
-        System.out.println("Number occurrences: " + numberOccurrences);
+        int numberOccurrences = 0; 
+        int result = countNumberOccurrences( number,  numbers);
+
+        System.out.println("Number occurrences: " + result);
     }
 
     public static int countNumberOccurrences(int number, int[] numbers) {
-        // Implement method
-        return 0;
+      int counter= 0;
+         for (int i = 0; i < numbers.length; i++){
+          if(number == numbers[i]){
+          counter++;
+          }
+         }
+        return counter;
+    
+         
+
     }
 }
+
--- src/exercise7/TimeValidation.java
+++ src/exercise7/TimeValidation.java
@@ -8,23 +8,39 @@ public class TimeValidation {
 
         System.out.print("Enter hours: ");
         int hours = input.nextInt();
-
+        
+        
         System.out.print("Enter minutes: ");
         int minutes = input.nextInt();
-
-        boolean timeValid = false; // Call validation methods
+        
+        boolean timeValid = false; 
+        boolean result1 = areHoursValid( hours);
+        
+        boolean result2 = areMinutesValid( minutes);
+        
+        if (result1 && result2 ){
+          timeValid = true;
+        }else{
+          timeValid = false;
+        }
         System.out.println("Time valid: " + timeValid);
     }
 
 
     public static boolean areHoursValid(int hours) {
-        // Implement method
-        return false;
+        if ( hours > 0 &&  hours < 24){
+        return true;
+        }else{
+          return false;
+        }
     }
 
 
     public static boolean areMinutesValid(int minutes) {
-        // Implement method
+       if (minutes > 0 && minutes < 60){
+         return true;
+       }else{
         return false;
     }
+}
 }
